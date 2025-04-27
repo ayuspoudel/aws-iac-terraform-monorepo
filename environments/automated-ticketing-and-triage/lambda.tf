7b972b20c1fdbd5527264659b59eee4e7a3d4d69
Ayush Poudel <ayushpoudel@usf.edu>
2025-04-27 19:58:22 -0400

    Lambda function update to use image uri

--- environments/automated-ticketing-and-triage/lambda.tf
+++ environments/automated-ticketing-and-triage/lambda.tf
@@ -1,3 +1,6 @@
+
+
+
 module "lambda_function" {
   source = "../../modules/lambda"
   function = local.lambda_function
@@ -7,7 +10,7 @@ module "lambda_function" {
   function_handler      = "jira_automation.lambda_handler"  # Use the handler inside the Docker image
 
   # Point to your ECR image URI (this is the format of the URI)
-  function_source_path  = "905418168893.dkr.ecr.us-east-1.amazonaws.com/automated-ticketing-and-triage:latest"
+  image_uri = "905418168893.dkr.ecr.us-east-1.amazonaws.com/automated-ticketing-and-triage:latest"
 
   # Lambda role ARN
   lambda_role_arn       = module.iam.iam_role_arn
