2d0239e0f191b38a44c90a7c592a95a7a1887d3b
Ayush Poudel <ayushpoudel@usf.edu>
2025-04-27 19:55:09 -0400

    Lambda

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -2,8 +2,8 @@ resource "aws_lambda_function" "this" {
   function_name = local.function_name
   runtime       = var.function_runtime
   handler       = var.function_handler
-  filename      = var.function_source_path
+  # filename      = var.function_source_path
   role          = var.lambda_role_arn  # Assume you pass the IAM Role ARN separately (I'll explain this below)
-
+  image_uri     = var.image_uri
   tags = local.all_tags
 }
