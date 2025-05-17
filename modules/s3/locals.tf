2d61c002d618a22679baf00f78a5804f22289b2f
Ayush Poudel <ayushpoudel@usf.edu>
2025-05-17 15:27:18 -0400

    Update bucket name to include timestamp for uniqueness

--- modules/s3/locals.tf
+++ modules/s3/locals.tf
@@ -1,5 +1,8 @@
 locals {
-  s3_bucket_name = "${var.project}-${var.owner}-bucket"
+  raw_timestamp = timestamp()                            # e.g. 2025-05-17T12:34:56Z
+  date_only     = substr(local.raw_timestamp, 0, 10)    # e.g. "2025-05-17"
+  clean_date    = replace(local.date_only, "-", "")     # e.g. "20250517"
+  s3_bucket_name = "${var.project}-${var.owner}-bucket-${locals.clean_date}"
 
   reserved_tags = {
     Owner        = var.owner
