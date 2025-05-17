ba266321feb8e2ad18f22b38ec398f188e1d2b95
Ayush Poudel <ayushpoudel@usf.edu>
2025-05-17 17:37:50 -0400

    Error fix

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -36,20 +36,6 @@ resource "aws_subnet" "private" {
   tags = merge(local.merged_tags, { Name = local.private_subnet_names[count.index] })
 }
 
-# Elastic IPs for NAT gateways
-resource "aws_eip" "nat" {
-  count = var.enable_nat_gateway ? (var.nat_gateway_per_az ? length(var.availability_zones) : 1) : 0
-  vpc   = true
-
-  tags = merge(
-    local.merged_tags,
-    { Name = var.nat_gateway_per_az ?
-      local.nat_gateway_names[count.index] + "-eip" :
-      "${var.project_name}-nat-gateway-eip"
-    }
-  )
-}
-
 # Elastic IPs for NAT Gateways
 resource "aws_eip" "nat" {
   count = var.enable_nat_gateway ? (var.nat_gateway_per_az ? length(var.availability_zones) : 1) : 0
@@ -58,7 +44,9 @@ resource "aws_eip" "nat" {
   tags = merge(
     local.merged_tags,
     {
-      Name = var.nat_gateway_per_az ? "${var.project_name}-eip-nat-${count.index}" : "${var.project_name}-eip-nat"
+      Name = var.nat_gateway_per_az ?
+        local.nat_gateway_names[count.index] + "-eip" :
+        "${var.project_name}-nat-gateway-eip"
     }
   )
 }
@@ -67,8 +55,7 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "this" {
   count         = var.enable_nat_gateway ? (var.nat_gateway_per_az ? length(var.availability_zones) : 1) : 0
   allocation_id = aws_eip.nat[count.index].id
-
-  subnet_id = var.nat_gateway_per_az ? aws_subnet.public[count.index].id : aws_subnet.public[0].id
+  subnet_id     = var.nat_gateway_per_az ? aws_subnet.public[count.index].id : aws_subnet.public[0].id
 
   tags = merge(
     local.merged_tags,
@@ -79,8 +66,7 @@ resource "aws_nat_gateway" "this" {
   )
 }
 
-
-# Route Tables and Associations for Public Subnets
+# Public Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
 
@@ -99,14 +85,7 @@ resource "aws_route_table_association" "public_subnet_association" {
   route_table_id = aws_route_table.public.id
 }
 
-# Route Tables and Associations for Private Subnets
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.this.id
-
-  tags = merge(local.merged_tags, { Name = "${var.project_name}-private-rt" })
-}
-
-# Private route to NAT Gateway
+# Private Route Tables
 resource "aws_route_table" "private" {
   count  = length(var.availability_zones)
   vpc_id = aws_vpc.this.id
@@ -115,10 +94,10 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route" "private_nat_gateway" {
-  count                 = var.enable_nat_gateway ? length(var.availability_zones) : 0
-  route_table_id        = aws_route_table.private[count.index].id
+  count                  = var.enable_nat_gateway ? length(var.availability_zones) : 0
+  route_table_id         = aws_route_table.private[count.index].id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id        = aws_nat_gateway.this[count.index].id
+  nat_gateway_id         = aws_nat_gateway.this[count.index].id
 }
 
 resource "aws_route_table_association" "private_subnet_association" {
@@ -127,27 +106,20 @@ resource "aws_route_table_association" "private_subnet_association" {
   route_table_id = aws_route_table.private[count.index].id
 }
 
-
-resource "aws_route_table_association" "private_subnet_association" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
-}
-
 # VPN Gateway (optional)
 resource "aws_vpn_gateway" "this" {
   count  = var.enable_vpn_gateway ? 1 : 0
   vpc_id = aws_vpc.this.id
 
-  tags = merge(local.merged_tags, { Name = local.vpn_gateway_name })
+  tags = merge(local.merged_tags, { Name = "${var.project_name}-vpn-gateway" })
 }
 
 # Transit Gateway Attachment (optional)
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
-  count             = var.enable_transit_gateway ? 1 : 0
+  count              = var.enable_transit_gateway ? 1 : 0
   transit_gateway_id = var.transit_gateway_id
-  vpc_id            = aws_vpc.this.id
-  subnet_ids        = aws_subnet.private[*].id
+  vpc_id             = aws_vpc.this.id
+  subnet_ids         = aws_subnet.private[*].id
 
-  tags = merge(local.merged_tags, { Name = local.transit_gateway_attachment_name })
+  tags = merge(local.merged_tags, { Name = "${var.project_name}-tgw-attachment" })
 }
