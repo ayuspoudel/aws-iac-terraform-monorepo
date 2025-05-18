1adcceef18efa6804b4950e612f4f03f70a187c1
Ayush Poudel <ayushpoudel@usf.edu>
2025-05-18 16:50:06 -0400

    Update outputs

--- modules/security-group/data.tf
+++ modules/security-group/data.tf
@@ -1,3 +1,29 @@
-data "external" "public_ip" {
-  program = ["bash", "${path.root}/scripts/get_my_ip.sh"]
+output "security_group_id" {
+  description = "ID of the security group created"
+  value       = var.enabled && var.create_security_group ? aws_security_group.this[0].id : null
+}
+
+output "security_group_arn" {
+  description = "ARN of the security group created"
+  value       = var.enabled && var.create_security_group ? aws_security_group.this[0].arn : null
+}
+
+output "security_group_name" {
+  description = "Name of the security group created or referenced"
+  value       = var.name
+}
+
+output "ingress_rule_count" {
+  description = "Number of ingress rules applied"
+  value       = length(var.ingress_rules)
+}
+
+output "egress_rule_count" {
+  description = "Number of egress rules applied"
+  value       = length(var.egress_rules)
+}
+
+output "tags" {
+  description = "Tags applied to the security group"
+  value       = var.tags
 }
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -8,16 +8,15 @@ output "vpc_cidr_block" {
   value       = aws_vpc.this.cidr_block
 }
 
-output "public_subnet_ids" {
+output "public_subnets" {
   description = "List of public subnet IDs"
   value       = aws_subnet.public[*].id
 }
 
-output "private_subnet_ids" {
+output "private_subnets" {
   description = "List of private subnet IDs"
   value       = aws_subnet.private[*].id
 }
-
 
 output "nat_gateway_ids" {
   description = "List of NAT Gateway IDs"
