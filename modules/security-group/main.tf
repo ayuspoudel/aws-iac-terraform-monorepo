97c3b39be963f570945aeb9b9d99b424237a68fd
Ayush Poudel <ayushpoudel@usf.edu>
2025-05-18 22:52:29 -0400

    Ady dynamic ipv6, self = true and cidr_blocks declarations

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -6,7 +6,6 @@ resource "aws_security_group" "this" {
   revoke_rules_on_delete  = var.revoke_rules_on_delete
   tags                    = var.tags
 }
-
 resource "aws_security_group_rule" "ingress" {
   for_each = local.expanded_ingress_rules
 
@@ -14,12 +13,36 @@ resource "aws_security_group_rule" "ingress" {
   from_port         = each.value.from_port
   to_port           = each.value.to_port
   protocol          = each.value.protocol
-  description       = try(each.value.description, local.predefined_rules[each.key].description)
-  cidr_blocks       = try(each.value.cidr_blocks, var.default_cidr_blocks)
-  ipv6_cidr_blocks  = try(each.value.ipv6_cidr_blocks, [])
-  prefix_list_ids   = try(each.value.prefix_list_ids, [])
+  description       = try(each.value.description, "")
   security_group_id = local.this_sg_id
-  self              = try(each.value.self, false)
+
+  dynamic "self" {
+    for_each = try(each.value.self, false) ? [1] : []
+    content {
+      self = true
+    }
+  }
+
+  dynamic "cidr_blocks" {
+    for_each = try(each.value.self, false) ? [] : [1]
+    content {
+      cidr_blocks = try(each.value.cidr_blocks, var.default_cidr_blocks)
+    }
+  }
+
+  dynamic "ipv6_cidr_blocks" {
+    for_each = try(each.value.self, false) ? [] : [1]
+    content {
+      ipv6_cidr_blocks = try(each.value.ipv6_cidr_blocks, [])
+    }
+  }
+
+  dynamic "prefix_list_ids" {
+    for_each = try(each.value.self, false) ? [] : [1]
+    content {
+      prefix_list_ids = try(each.value.prefix_list_ids, [])
+    }
+  }
 
   lifecycle {
     create_before_destroy = true
@@ -35,19 +58,44 @@ resource "aws_security_group_rule" "ingress" {
 
 
 
-resource "aws_security_group_rule" "engress" {
+
+resource "aws_security_group_rule" "egress" {
   for_each = local.expanded_egress_rules
 
-  type              = "ingress"
+  type              = "egress"
   from_port         = each.value.from_port
   to_port           = each.value.to_port
   protocol          = each.value.protocol
-  description       = try(each.value.description, local.predefined_rules[each.key].description)
-  cidr_blocks       = try(each.value.cidr_blocks, var.default_cidr_blocks)
-  ipv6_cidr_blocks  = try(each.value.ipv6_cidr_blocks, [])
-  prefix_list_ids   = try(each.value.prefix_list_ids, [])
+  description       = try(each.value.description, "")
   security_group_id = local.this_sg_id
-  self              = try(each.value.self, false)
+
+  dynamic "self" {
+    for_each = try(each.value.self, false) ? [1] : []
+    content {
+      self = true
+    }
+  }
+
+  dynamic "cidr_blocks" {
+    for_each = try(each.value.self, false) ? [] : [1]
+    content {
+      cidr_blocks = try(each.value.cidr_blocks, var.default_cidr_blocks)
+    }
+  }
+
+  dynamic "ipv6_cidr_blocks" {
+    for_each = try(each.value.self, false) ? [] : [1]
+    content {
+      ipv6_cidr_blocks = try(each.value.ipv6_cidr_blocks, [])
+    }
+  }
+
+  dynamic "prefix_list_ids" {
+    for_each = try(each.value.self, false) ? [] : [1]
+    content {
+      prefix_list_ids = try(each.value.prefix_list_ids, [])
+    }
+  }
 
   lifecycle {
     create_before_destroy = true
