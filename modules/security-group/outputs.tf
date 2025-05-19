4297b9ebf772f36a8e3c41eef281236389c52993
Ayush Poudel <ayushpoudel@usf.edu>
2025-05-18 22:48:51 -0400

    Validation to outputs

--- modules/security-group/outputs.tf
+++ modules/security-group/outputs.tf
@@ -1,29 +1,29 @@
 output "security_group_arn" {
   description = "The ARN of the security group"
-  value       = try(aws_security_group.this[0].arn, aws_security_group.this_name_prefix[0].arn, "")
+  value       = var.enabled && var.create_security_group ? aws_security_group.this[0].arn : null
 }
 
 output "security_group_id" {
   description = "The ID of the security group"
-  value       = try(aws_security_group.this[0].id, aws_security_group.this_name_prefix[0].id, "")
+  value       = var.enabled && var.create_security_group ? aws_security_group.this[0].id : null
 }
 
 output "security_group_vpc_id" {
-  description = "The VPC ID"
-  value       = try(aws_security_group.this[0].vpc_id, aws_security_group.this_name_prefix[0].vpc_id, "")
+  description = "The VPC ID where the SG resides"
+  value       = var.enabled && var.create_security_group ? aws_security_group.this[0].vpc_id : null
 }
 
 output "security_group_owner_id" {
-  description = "The owner ID"
-  value       = try(aws_security_group.this[0].owner_id, aws_security_group.this_name_prefix[0].owner_id, "")
+  description = "The AWS account ID of the SG owner"
+  value       = var.enabled && var.create_security_group ? aws_security_group.this[0].owner_id : null
 }
 
 output "security_group_name" {
   description = "The name of the security group"
-  value       = try(aws_security_group.this[0].name, aws_security_group.this_name_prefix[0].name, "")
+  value       = var.enabled && var.create_security_group ? aws_security_group.this[0].name : null
 }
 
 output "security_group_description" {
   description = "The description of the security group"
-  value       = try(aws_security_group.this[0].description, aws_security_group.this_name_prefix[0].description, "")
+  value       = var.enabled && var.create_security_group ? aws_security_group.this[0].description : null
 }
